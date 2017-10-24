9cb71000a1fc00526a02c8e2ba86d980ab28b70d
Crista Cooper <aine3d@gmail.com>
2017-10-24 18:22:37 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,24 @@
-//define functions here
+function getIt() {
+  $('p').on("click", function () {
+    alert("Hey!");
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function () {
+    $("img").addClass("tasty");
+  })
+}
+
+function pressIt() {
+  $('#typing').on('keydown', function(press) {
+    if(press.which == 71){
+      alert('g was pressed');
+    }
+  });
+}
 
 $(document).ready(function(){
 
-// call functions here
 
 });
